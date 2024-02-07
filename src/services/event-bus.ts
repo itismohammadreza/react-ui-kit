e0a36319b39c156c2e1efac79e81cfed65e64895
Mohammadreza <shahbaziweb@gmail.com>
2024-02-07 12:47:02 +0330

    create event-bus service

---
+++ src/services/event-bus.ts
@@ -0,0 +1,35 @@
+import {useEffect} from "react";
+
+export function EventBus() {
+  const allHandlers = new Map();
+
+  return {
+    on: (type, handler) => {
+      let handlers = allHandlers.get(type);
+
+      if (!handlers) handlers = [handler];
+      else handlers.push(handler);
+
+      allHandlers.set(type, handlers);
+    },
+    off: (type, handler) => {
+      let handlers = allHandlers.get(type);
+
+      handlers && handlers.splice(handlers.indexOf(handler) >>> 0, 1);
+    },
+    emit: (type, evt) => {
+      let handlers = allHandlers.get(type);
+
+      handlers && handlers.slice().forEach((handler) => handler(evt));
+    },
+  };
+}
+
+// Usage:
+// const eventService = EventBus();
+// useEffect(() => {
+//   eventService.on("customEvent", (e) => console.log(e));
+//   setTimeout(() => {
+//     eventService.emit("customEvent", {visible: true});
+//   }, 3000);
+// }, []);
