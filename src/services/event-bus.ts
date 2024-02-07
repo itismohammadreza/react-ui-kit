bc951052ef9a57ddddb5cfbcf1599afe07485776
Mohammadreza <shahbaziweb@gmail.com>
2024-02-07 12:54:15 +0330

    fix linters in event-bus service

--- src/services/event-bus.ts
+++ src/services/event-bus.ts
@@ -1,26 +1,27 @@
 import {useEffect} from "react";
 
-export function EventBus() {
-  const allHandlers = new Map();
+type HandlerFn = (_?: any) => any;
+
+export const EventBus = () => {
+  const allHandlers = new Map<string, HandlerFn[]>();
 
   return {
-    on: (type, handler) => {
+    on: (type: string, handler: HandlerFn) => {
       let handlers = allHandlers.get(type);
-
-      if (!handlers) handlers = [handler];
-      else handlers.push(handler);
-
+      if (!handlers) {
+        handlers = [handler];
+      } else {
+        handlers.push(handler);
+      }
       allHandlers.set(type, handlers);
     },
-    off: (type, handler) => {
-      let handlers = allHandlers.get(type);
-
+    off: (type: string, handler: HandlerFn) => {
+      const handlers = allHandlers.get(type);
       handlers && handlers.splice(handlers.indexOf(handler) >>> 0, 1);
     },
-    emit: (type, evt) => {
-      let handlers = allHandlers.get(type);
-
-      handlers && handlers.slice().forEach((handler) => handler(evt));
+    emit: (type: string, event: any) => {
+      const handlers = allHandlers.get(type);
+      handlers && handlers.slice().forEach((handler) => handler(event));
     },
   };
 }
